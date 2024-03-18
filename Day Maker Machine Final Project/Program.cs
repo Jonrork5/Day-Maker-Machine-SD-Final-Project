31dbead0f1c1ec5f4fe9a35fe9e77c626fb37e41
Jonrork5 <jonrork5@gmail.com>
2024-03-17 22:46:50 -0400

    day maker machine

---
+++ Day Maker Machine Final Project/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DayMakerMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Welcome to the Day Maker Machine! A.K.A Compliment Slots!");
+            Console.WriteLine("Please type SPIN and hit enter to get a boost of self-confidence!");
+
+            string userInput = Console.ReadLine();
+
+            if (userInput.ToUpper() == "SPIN")
+            {
+                // Call a method to generate and display the compliment
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please type SPIN and hit enter.");
+            }
+        }
+    }
+}
